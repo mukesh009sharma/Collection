3ceaec1a0e46a27ce1e3d9877ac5d5d40250fd49
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-10-03 22:04:30 +0530

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,38 @@
+//Q1.Create a RemoteControl interface with methods turnOn() and turnOff(). Implement this interface in TV and Fan classes, showing how they can be controlled using these methods. 
+interface RemoteControl {
+    void turnOn();
+    void turnOff();
+}
+
+class TV implements RemoteControl {
+    public void turnOn() {
+        System.out.println("The TV is now ON.");
+    }
+
+    public void turnOff() {
+        System.out.println("The TV is now OFF.");
+    }
+}
+
+class Fan implements RemoteControl {
+    public void turnOn() {
+        System.out.println("The Fan is now ON.");
+    }
+
+    public void turnOff() {
+        System.out.println("The Fan is now OFF.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        RemoteControl tv = new TV();
+        RemoteControl fan = new Fan();
+
+        tv.turnOn();
+        fan.turnOn();
+
+        tv.turnOff();
+        fan.turnOff();
+    }
+}
